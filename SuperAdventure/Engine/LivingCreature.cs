a7174d4d83b79eb0b74b3e008f5aa809cdd1a913
Xander Houtman <xander.houtman@yacht.nl>
2018-04-06 09:30:23 +0200

    fixed an oopsie

--- SuperAdventure/Engine/LivingCreature.cs
+++ SuperAdventure/Engine/LivingCreature.cs
@@ -9,7 +9,7 @@ namespace Engine
     public class LivingCreature
     {
         private int _maxHitPoints;
-        public int MaxHitPoints { get { return _maxHitPoints; }; set { _maxHitPoints = value; HitPointsChanged?.Invoke(value); }; }
+        public int MaxHitPoints { get { return _maxHitPoints; } set { _maxHitPoints = value; HitPointsChanged?.Invoke(value); } }
         public int CurrentHitPoints { get; set; }
 
         public event Action<int> HitPointsChanged;
